a891dffbd84f93970caabf4e595d5634980a04ba
Thet Hnin Aye <thaye.thethnin@gmail.com>
2018-04-03 00:59:34 +0800

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,16 @@
 '''Thet Hnin Aye'''
+
+def main():
+
+name = (input("Enter your name here "))
+
+#This is a comment
+
+
+while len(name) <= 1:
+    print("Error")
+    name = (input("Enter your name here "))
+
+print(name[::2])
+
+main()
